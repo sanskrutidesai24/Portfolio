d0e54e4bb1adacae60ec3a3a8895ba2cc7eed582
Sanskruti Desai <sanskrutidesai24@gmail.com>
2026-08-20 22:53:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+document.querySelectorAll('a[href^="#"]').forEach(a=>a.addEventListener("click",e=>{const t=document.querySelector(a.getAttribute("href"));if(t){e.preventDefault();t.scrollIntoView({behavior:"smooth"});document.querySelector("nav").classList.remove("open")}}));
+document.querySelector(".menu-btn").addEventListener("click",()=>document.querySelector("nav").classList.toggle("open"));
